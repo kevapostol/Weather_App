9944281dde7f825cad7056175d30116f5621b590
Kevin Apostol <apostolkevin@gmail.com>
2020-09-01 16:34:10 -0700

    Added functionality to the showWeatherData function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  * Weather App
  * DONE: Complete getWeatherData() to return json response Promise
  * DONE: Complete searchCity() to get user input and get data using getWeatherData()
- * TODO: Complete showWeatherData() to set the data in the the html file from response
+ * DONE: Complete showWeatherData() to set the data in the the html file from response
  */
 
 // API_KEY for maps api
@@ -26,6 +26,7 @@ getWeatherData = ( city ) => {
 searchCity = () => {
     const city = document.getElementById( 'city-input' ).value;
     getWeatherData(city).then((response) => {
+        showWeatherData(response);
         console.log(response);
     }).catch((error) => {
         console.log(error);
@@ -34,10 +35,12 @@ searchCity = () => {
 
 /**
  * Show the weather data in HTML
- * HINT: make sure to console log the weatherData to see how the data looks like
  */
 showWeatherData = ( weatherData ) => {
-    //CODE GOES HERE
+    document.getElementById( "city-name" ).innerText = weatherData.name;
+    document.getElementById( "weather-type" ).innerText = weatherData.weather[0].main;
+    document.getElementById( "temp" ).innerText = weatherData.main.temp;
+    document.getElementById( "min-temp" ).innerText = weatherData.main.temp_min;
+    document.getElementById( "max-temp" ).innerText = weatherData.main.temp_max;
 
 }
-
