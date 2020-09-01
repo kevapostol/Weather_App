723b41d0ebd4a372c377adc02ab7011b9650db9a
Kevin Apostol <apostolkevin@gmail.com>
2020-09-01 14:56:32 -0700

    Updated the searchCity function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /**
  * Weather App
  * DONE: Complete getWeatherData() to return json response Promise
- * TODO: Complete searchCity() to get user input and get data using getWeatherData()
+ * DONE: Complete searchCity() to get user input and get data using getWeatherData()
  * TODO: Complete showWeatherData() to set the data in the the html file from response
  */
 
@@ -14,22 +14,22 @@ let API_KEY = "725e1166e8766a8cd281fef948b51818";
 getWeatherData = ( city ) => {
     const URL = "https://api.openweathermap.org/data/2.5/weather?";
     const FULL_URL = `${URL}q=${city}&appid=${API_KEY}&units=imperial`
-    //CODE GOES HERE
     const weatherPromise = fetch(FULL_URL);
     return weatherPromise.then((response) => {
         return response.json();
     })
 }
 
-console.log(getWeatherData("Detroit"));
 /**
  * Retrieve city input and get the weather data
- * HINT: Use the promise returned from getWeatherData()
  */
 searchCity = () => {
     const city = document.getElementById( 'city-input' ).value;
-    // CODE GOES HERE
-
+    getWeatherData(city).then((response) => {
+        console.log(response);
+    }).catch((error) => {
+        console.log(error);
+    })
 }
 
 /**
